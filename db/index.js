1fcefa0a270b2567609249fc92b604523c5bb203
Ivan Sabido <isabido@Ivans-MacBook-Pro.local>
2018-06-28 09:50:38 -0500

    npm lint command

--- db/index.js
+++ db/index.js
@@ -5,6 +5,3 @@
  * email: <isc_86@hotmail.com>
  * description: Módulo que se encarga de inicializar las conexiones, autenticar con la bd y realizar migraciones dependiendo de la variable de entorno NODE_ENV
  */
-
- 
-
